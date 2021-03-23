22fb0173d31d2c991f7f0722e9444d3ef83c888a
RENZO <rbedini@miuandes.cl>
2021-03-23 14:37:09 -0300

    third commit

--- lab1oficial.py
+++ lab1oficial.py
@@ -9,12 +9,15 @@ from random import randint, random, sample
 cards = int(input("How many pair of cards do you want to play? "))
 player1=0
 player2=0
+
 def create_real_table(cards):
     lista = range(1,1+cards)
     fila1=sample(lista, cards)
     fila2 = sample(lista, cards)
     matriz=np.array([fila1,fila2])
     print(matriz)
+    return(matriz)
+
 def create_censored_table(cards):
     lista2=[]
     lista3=[]
@@ -24,15 +27,26 @@ def create_censored_table(cards):
         lista3.append('?')
         count+=1
     matriz_censored=np.array([lista2,lista3])
-    print(matriz_censored)
-create_real_table(cards)
-create_censored_table(cards)
+    return(matriz_censored)
 
-"""       
-x= int(input("Tell me the coordinate in x "))
-while x<0 or x>cards-1:
-    print ("Please, choose another coordinate")
+
+def first_move(cards):
     x= int(input("Tell me the coordinate in x "))
-y= int(input("Choose a coordinate in y between -1 and 0 "))
-print(matriz[-y][x])
-"""
+    while x<0 or x>cards-1:
+        print ("Please, choose another coordinate")
+        x= int(input("Tell me the coordinate in x "))
+    y= int(input("Choose a coordinate in y between -1 and 0 "))
+    matriz_visible = create_real_table(cards)
+    board = create_censored_table(cards)
+    board[-y][x] = matriz_visible[-y][x]
+    print(board)
+    return(board)
+first_move(cards)
+print(matriz_visible)
+
+
+    
+    
+
+    
+        
