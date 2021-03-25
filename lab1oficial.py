ad33a640667d82d13c6cad519197113229ca06f6
RENZO <rbedini@miuandes.cl>
2021-03-25 11:13:36 -0300

    fourth commit

--- lab1oficial.py
+++ lab1oficial.py
@@ -6,10 +6,6 @@ Created on Tue Mar 23 10:07:43 2021
 """
 import numpy as np
 from random import randint, random, sample
-cards = int(input("How many pair of cards do you want to play? "))
-player1=0
-player2=0
-
 def create_real_table(cards):
     lista = range(1,1+cards)
     fila1=sample(lista, cards)
@@ -28,23 +24,85 @@ def create_censored_table(cards):
         count+=1
     matriz_censored=np.array([lista2,lista3])
     return(matriz_censored)
-
-
-def first_move(cards):
-    x= int(input("Tell me the coordinate in x "))
-    while x<0 or x>cards-1:
+cards = int(input("How many pair of cards do you want to play? "))
+player1=0
+player2=0
+matriz_visible=create_real_table(cards)
+board=create_censored_table(cards)
+truth=True
+"""
+def movement(cards,board):
+ """
+while (player1+player2)!=cards: 
+    print("It´s your turn player 1")
+    x1= int(input("Tell me the coordinate in x "))
+    while x1<0 or x1>cards-1:
         print ("Please, choose another coordinate")
-        x= int(input("Tell me the coordinate in x "))
-    y= int(input("Choose a coordinate in y between -1 and 0 "))
-    matriz_visible = create_real_table(cards)
-    board = create_censored_table(cards)
-    board[-y][x] = matriz_visible[-y][x]
+        x1= int(input("Tell me the coordinate in x "))
+    y1= int(input("Choose a coordinate in y between -1 and 0 "))
+    board[-y1][x1] = matriz_visible[-y1][x1]
+    number1=matriz_visible[-y1][x1]
     print(board)
-    return(board)
-first_move(cards)
+    x2= int(input("Tell me the coordinate in x "))
+    while x2<0 or x2>cards-1:
+        print ("Please, choose another coordinate")
+        x2= int(input("Tell me the coordinate in x "))
+    y2= int(input("Choose a coordinate in y between -1 and 0 "))
+    board[-y2][x2] = matriz_visible[-y2][x2]
+    number2=matriz_visible[-y2][x2]
+    print(board)
+    if number1==number2:
+        board[-y1][x1]=' '
+        board[-y2][x2]=' '
+        player1+=1
+        print("You made it")
+    else:
+        print("It´s your turn player 2")
+        x1= int(input("Tell me the coordinate in x "))
+        while x1<0 or x1>cards-1:
+            print ("Please, choose another coordinate")
+            x1= int(input("Tell me the coordinate in x "))
+        y1= int(input("Choose a coordinate in y between -1 and 0 "))
+        board[-y1][x1] = matriz_visible[-y1][x1]
+        number1=matriz_visible[-y1][x1]
+        print(board)
+        x2= int(input("Tell me the coordinate in x "))
+        while x2<0 or x2>cards-1:
+            print ("Please, choose another coordinate")
+            x2= int(input("Tell me the coordinate in x "))
+        y2= int(input("Choose a coordinate in y between -1 and 0 "))
+        board[-y2][x2] = matriz_visible[-y2][x2]
+        number2=matriz_visible[-y2][x2]
+        print(board)
+        if number1==number2:
+            board[-y1][x1]=' '
+            board[-y2][x2]=' '
+            player2+=1
+       
+    
+
+
+
+
+
+"""
+matriz_visible= create_real_table(cards)
+board= first_move(cards)
 print(matriz_visible)
 
-
+cards = int(input("How many pair of cards do you want to play? "))
+player1=0
+player2=0
+matriz_visible=create_real_table(cards)
+board=create_censored_table(cards)
+a=0
+truth=True
+while a!=2:
+    print("It´s your turn player 1")
+    movement(cards,board)
+"""
+    
+    
     
     
 
